21dfd6bd23730eddfd9e67a02b6593829536a2c8
Lonny Adarys <ladarys@oa-j3.msk.21-school.ru>
2021-12-29 20:43:23 +0300

    finish

--- includes/minitalk.h
+++ includes/minitalk.h
@@ -4,19 +4,17 @@
 # include <signal.h>
 # include "../libft/libft.h"
 
-#include <stdio.h>
-
-typedef struct	s_send
+typedef struct s_send
 {
 	int		pid;
 	char	*mess;
 	int		size;
 }				t_send;
 
-typedef struct	s_get
+typedef struct s_get
 {
 	char	*mess;
-	char 	chr;
+	char	chr;
 	int		size;
 	int		start;
 }				t_get;
