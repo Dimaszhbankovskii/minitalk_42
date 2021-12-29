e59f11244256a1c7824d3cffdb6e2ca3b5faa723
DimasZhbankovskii <84144634+Dimaszhbankovskii@users.noreply.github.com>
2021-12-29 11:04:43 +0300

    Delete bonus directory

--- bonus/libft/ft_isascii.c
+++
@@ -1,6 +0,0 @@
-#include "libft.h"
-
-int	ft_isascii(int character)
-{
-	return (character >= 0x00 && character <= 0x7F);
-}
--- bonus/libft/ft_tolower.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-
-int	ft_tolower(int character)
-{
-	if (character >= 'A' && character <= 'Z')
-		return (character + 32);
-	else
-		return (character);
-}
